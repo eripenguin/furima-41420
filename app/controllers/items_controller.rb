880b1e36ecd5f8d1e8f0e41d1214248a007ec1de
eripenguin <eri.assertion@gmail.com>
2025-05-03 20:00:22 +0900

    商品詳細表示機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit]
+
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -11,6 +13,13 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+    if @item.user_id == current_user.id && @item.order.nil?
+    else
+      redirect_to root_path
+    end
   end
   
   def create
@@ -29,5 +38,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end  
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
   
 end
